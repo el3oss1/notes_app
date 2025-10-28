5955945172a824bd58a63607dbde85a48390e457
el3oss1 <mustafaeliwa586@gmail.com>
2025-10-28 07:29:41 +0300

    create edite note view

---
+++ lib/view/widgets/edit_note_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditViewBody extends StatelessWidget {
+  const EditViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
